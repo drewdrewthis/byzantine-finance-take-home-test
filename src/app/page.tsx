4321a7903f0b1760b30bad66d6e01ea8935b3afe
Andrew Garde Joia <drewdrewthis@gmail.com>
2025-01-03 14:19:54 +0100

    Add searchbar filtering (#1)
    * Use shadcn and tailwind to set up searchbar
    
    * Update filter function to include address
    
    * Update searchbar styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 "use client";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import styles from "./page.module.scss";
 import TableDisplay from "@/components/TableDisplay/TableDisplay";
 import { VaultToDisplay } from "@/types/Table";
 import Link from "next/link";
 import dataTable from "@/data/dataTable.json";
+import { SearchBar } from "../components/SearchBar";
 
 export default function Home() {
   const [listVaultToDisplay, setListVaultToDisplay] = useState<
     VaultToDisplay[]
   >(dataTable as VaultToDisplay[]);
+
+  /**
+   * Test notes:
+   * This is a simple search call back that will filter the vaults by name or address based on the search input.
+   * If there were a lot of vaults, we'd consider offloading this to the backend by sending query parameters to the API.
+   */
+  const handleInputChange = useCallback ((input: string) => {
+    const filterFn = (vault: VaultToDisplay) => vault.name.toLowerCase().includes(input.toLowerCase()) || vault.address.toLowerCase().includes(input.toLowerCase());
+    setListVaultToDisplay((dataTable as VaultToDisplay[]).filter(filterFn));
+  }, []);
+
   return (
     <section className={styles.page}>
       <div className={styles.explanation}>
@@ -30,9 +42,9 @@ export default function Home() {
         </p>
       </div>
       <div className={styles.tableContainer}>
-        <div className={styles.nameAndSearch}>
+        <div className="flex justify-between w-full items-center">
           <h2>The vaults</h2>
-          <input type="text" placeholder="This is what you need to replace" />
+          <SearchBar onInputChange={handleInputChange} />
         </div>
         <TableDisplay data={listVaultToDisplay} />
       </div>
