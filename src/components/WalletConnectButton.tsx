63951fc07cdf495e6f1b8fead3a7edcf2d413604
drewdrew <drewdrewthis@gmail.com>
2025-01-03 14:42:10 +0100

    Style color tweaks from original to make things visible

--- src/components/WalletConnectButton.tsx
+++ src/components/WalletConnectButton.tsx
@@ -2,6 +2,14 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 
 
+/**
+ * Test Note:
+ * 
+ * This is just using the default RainbowKit ConnectButton and is unstyled.
+ * 
+ * We'd need to style it to match the design, but in a production app
+ * we'd want to use a custom component for full control.
+ */
 export const WalletConnectButton = () => {
   return <ConnectButton />;
 };
