2da1381e5ec6f3559445b8591f8d4fc91678b565
drewdrew <drewdrewthis@gmail.com>
2025-01-03 17:26:26 +0100

    Clean up

--- src/contracts/byzETHVault.ts
+++ src/contracts/byzETHVault.ts
@@ -1,6 +1,5 @@
 import { BYZETH_VAULT_ABI } from "./byzETHVault.abi";
 
-// ABI for the main functions we need
 export const BYZETH_VAULT_ADDRESS = '0xf6ceae45825f961851917EFca73d35cF7601A88a';
 
 // Contract configuration
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,1 @@
-export * from './web3';  
 export * from './Table';
